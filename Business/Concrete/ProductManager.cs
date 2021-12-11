a696e6038dafc1e7734a600395f93f6244ac4a0c
Mertcan Karataş <can.mert.karatas@gmail.com>
2021-12-11 22:14:02 +0300

    First commit

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,56 @@
+﻿using Business.Abstract;
+using Business.Constant;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Model.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{ /// <summary>
+  /// iş katmanı
+  /// burada yapılacak işleri direk verdiğimiz kısım api tarafındaki controller'lardan direk burası ile çalışırız bu da ilgili işi yapmak için diğer katmanlara başvurur
+  /// SOLID presiplerinden S harfi (single of responsibility - Tek sorumluluk ilkesi)katı bir şekilde geçerlidir. Bir method bir iş  yapar. 
+  /// if suistimali yapılmamalıdır. 
+  /// Product ile ilgili Ekleme Silme Güncelleme Listeleme VB. DataAccess ile iletişim kurar ve gerçekleştirir.
+  /// </summary>
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public IResult Add(Product product)
+        {
+            _productDal.Add(product);
+            return new SuccessResult(Messages.ProductAdded);
+        }
+
+        public IResult Delete(Product product)
+        {
+            _productDal.Delete(product);
+            return new SuccessResult(Messages.ProductDeleted);
+        }
+
+        public IDataResult<Product> Get(int id)
+        {
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.Id == id));
+        }
+
+        public IDataResult<List<Product>> GetAll()
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
+        }
+
+        public IResult Update(Product product)
+        {
+            _productDal.Update(product);
+            return new SuccessResult(Messages.ProductUpdated);
+        }
+    }
+}
---
+++ WebAPI/Infrastructer/MappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Model.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Infrastructer
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserModel, User>();
+            CreateMap<User,UserModel>();
+
+            CreateMap<Category, CategoryModel>();
+            CreateMap<CategoryModel,Category>();
+            CreateMap<ProductModel,Product>();
+            CreateMap<Product,ProductModel>();
+
+        }
+    }
+}
